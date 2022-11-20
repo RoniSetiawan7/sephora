c77b003aabf22179339cfb66661be829ab02baa2
ronisetiawan7 <ronisetiawan0109@gmail.com>
2022-11-20 17:28:45 +0700

    menambahkan animasi pada halaman split bill

--- lib/notification/widgets/split_bill.dart
+++ lib/notification/widgets/split_bill.dart
@@ -64,93 +64,132 @@ class SplitBill extends StatelessWidget {
             const SizedBox(height: 12),
             Row(
               children: [
+                const SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: UserStack(),
+                ),
                 Expanded(
-                  child: Stack(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Positioned(
-                        left: 80,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: const BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
-                          ),
+                      const Text(
+                        'Nominal Tagihan',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 10,
+                          color: Color(0xFF97A5C9),
                         ),
                       ),
-                      Positioned(
-                        left: 60,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: const BoxDecoration(
-                            color: Colors.yellow,
-                            shape: BoxShape.circle,
-                          ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Rp ${NumberFormat('#,###', 'id_ID').format(amount)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Color(0xFF2B2F3C),
                         ),
-                      ),
-                      Positioned(
-                        left: 40,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: const BoxDecoration(
-                            color: Colors.green,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 20,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: const BoxDecoration(
-                            color: Colors.blue,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            width: 3,
-                            color: const Color(0XFF2F82FF),
-                          ),
-                        ),
-                      ),
+                      )
                     ],
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Nominal Tagihan',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 10,
-                        color: Color(0xFF97A5C9),
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      'Rp ${NumberFormat('#,###', 'id_ID').format(amount)}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Color(0xFF2B2F3C),
-                      ),
-                    )
-                  ],
                 )
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserStack extends StatefulWidget {
+  const UserStack({super.key});
+
+  @override
+  State<UserStack> createState() => _UserStackState();
+}
+
+class _UserStackState extends State<UserStack>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _moveRight;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    _moveRight = Tween<double>(
+      begin: 0,
+      end: 20,
+    ).animate(_animationController);
+
+    super.initState();
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+
+    _animationController.forward();
+  }
+
+  final users = [
+    <String, dynamic>{'image': 'https://picsum.photos/100?image=1'},
+    <String, dynamic>{'image': 'https://picsum.photos/250?image=2'},
+    <String, dynamic>{'image': 'https://picsum.photos/250?image=3'},
+    <String, dynamic>{'image': 'https://picsum.photos/250?image=4'},
+    <String, dynamic>{'image': 'https://picsum.photos/250?image=5'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: users.reversed.toList().asMap().entries.map((e) {
+        final index = e.key;
+        final value = e.value;
+        return Positioned(
+          left: _moveRightValue(index),
+          child: UserStackItem(
+            active: index == users.length - 1,
+            image: value['image'],
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  double _moveRightValue(int index) {
+    final x = _moveRight.value * (users.length - index) - 20;
+    return x < 0 ? 0 : x;
+  }
+}
+
+class UserStackItem extends StatelessWidget {
+  final bool active;
+  final String image;
+
+  const UserStackItem({
+    Key? key,
+    required this.active,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          width: 2,
+          color: active ? const Color(0XFF2F82FF) : Colors.white,
+        ),
+        image: DecorationImage(
+          image: NetworkImage(image),
+          fit: BoxFit.cover,
         ),
       ),
     );
